8402badb3254435615ef24b2357054d0708c29c5
gopal6830 <112460716+gopal6830@users.noreply.github.com>
2022-09-30 15:11:07 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 require_once("db.php");
 ?>
 <?php	
-	$pdo_statement = $pdo_conn->prepare("SELECT * FROM student ORDER BY id DESC");
+	$pdo_statement = $pdo_conn->prepare("SELECT * FROM student");
 	$pdo_statement->execute();
 	$result = $pdo_statement->fetchAll();
 	json_encode($result);
